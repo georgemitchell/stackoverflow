137c5bc696b307570d0de89d3ea8508c52016178
Thomas Kunambi <kunambi@users.noreply.github.com>
2021-09-15 19:19:01 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='timezone_to_locale',
+    version='0.0.1',
+    description='Convert timezone to locale',
+    packages=setuptools.find_packages()
+)
