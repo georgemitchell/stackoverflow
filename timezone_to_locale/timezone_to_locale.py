e93ef533175729da071a9ed482c25606068545dd
Thomas Kunambi <kunambi@users.noreply.github.com>
2021-09-16 01:37:16 +0200

    Flake8

--- timezone_to_locale/timezone_to_locale.py
+++ timezone_to_locale/timezone_to_locale.py
@@ -1,15 +1,12 @@
 from pytz import country_timezones
-from .language_codes import locale_by_country
-import re
+from timezone_to_locale.language_codes import locale_by_country
 
 locale_by_timezone = {}
 
-# Get a list of all the timezones by country
 country_by_timezone = {}
 for code in country_timezones:
     for timezone in country_timezones[code]:
-        # check that each timezone only belongs to one country
-        assert(timezone not in country_by_timezone)
+        assert timezone not in country_by_timezone
         country_by_timezone[timezone] = code
 
 for timezone in country_by_timezone:
@@ -18,7 +15,7 @@ for timezone in country_by_timezone:
         raise IndexError(f"Unable to find {country_code} in locale_by_country")
     else:
         languages = locale_by_country[country_code]["languages"]
-        locale_by_timezone[timezone] = [l.strip() for l in languages.split(",")]
+        locale_by_timezone[timezone] = [language.strip() for language in languages.split(",")]
 
 if __name__ == "__main__":
     timezone = "America/Chicago"
@@ -27,5 +24,4 @@ if __name__ == "__main__":
             raise IndexError(f"Unable to locate timezone: '{timezone}'")
         else:
             for i, language in enumerate(locale_by_timezone[timezone]):
-                print("%d) %s" % (i+1, language))
-        timezone = raw_input("Timezone: ")
+                print("%d) %s" % (i + 1, language))
